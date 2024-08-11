0cf4a52f67db725e5e2a291f3101628b36421902
mohamed3zz <elgendymohamed7y@gmail.com>
2024-08-11 23:39:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,152 @@
+//Q1:
+// var num=prompt('enter number');
+// if(num<=100)
+// {
+//     console.log('number is less than or equal to 100');
+// }
+// else 
+// {
+//     console.log('number is greater than 100');
+// }
+//--------------------------------------------------------------------------------------
+//Q2:
+// var string=prompt('enter text');
+// if (string.length>100)
+// {
+// console.log('string is long');
+// }
+// else {
+//     console.log('string is short');
+// }
+//------------------------------------------------------------------------------------
+//Q3:
+// var pass1 = prompt('enter new password');
+// var pass2 = prompt('confirm password');
+// if (pass1==pass2)
+// {
+//    console.log(' password is valid');
+// }
+// else 
+// {
+//     console.log(' password is not valid');
+// }
+//-----------------------------------------------------------------------------------
+//Q4:
+// var day = prompt('enter the day');
+// switch (day)
+// {
+//     case 'saturday' :
+//             console.log('it is weekend');
+//             break;
+//      case 'sunday' :
+//             console.log('work day');
+//             break;
+//      case 'monday' :
+//             console.log('work day');
+//             break;
+//     case 'tuesday' :
+//             console.log('work day');
+//             break;
+//     case 'wednesday' :
+//             console.log('work day');
+//             break;
+//     case 'thursday' :
+//             console.log('work day');
+//             break;
+//     case 'friday' :
+//             console.log('work day');
+//             break;
+//     default :
+//             console.log('enter valid day');
+// }
+//--------------------------------------------------------------------------------------
+//Q5:
+// var num = prompt("enter number");
+// if(num==0)
+// {
+//     console.log("number is zero")
+// }
+// else
+// {
+// if (num>0)
+// {
+//     console.log("number is positive");
+// }
+// else 
+// {
+//     console.log("number is negative");
+// }
+// }
+//-----------------------------------------------------------------------------------------
+//Q6:
+// let grade = prompt("enter your grade")
+// if (grade>=90)
+// {
+//     console.log('A');
+// }
+// else if (grade>=80 && grade<90)
+// {
+//     console.log('B');
+// }
+// else if (grade>=70 && grade<80)
+//     {
+//         console.log('C');
+//     }
+// else if (grade>=50 && grade<70)
+//         {
+//             console.log('D');
+//         }
+// else if (grade<50)
+//     {
+//         console.log('F');
+//     }
+//--------------------------------------------------------------------------------------------
+//Q7:
+// let temp = prompt("enter the temperature");
+// if (temp>30)
+//     console.log("it is hot");
+// else 
+// console.log("it is cold");
+//---------------------------------------------------------------------------------------------
+//Q8:
+// function knowOddOrEven(num)
+// {
+//     if (num==0)
+//     {
+//         console.log('number is zero')
+//     }
+//     else {
+//         switch (num%2==0) 
+//         {
+//    case true :
+//    console.log('number is even');
+//    break;
+//    case false :
+//    console.log('number is odd');
+//    break;
+//         }
+//     }
+// }
+// let x = prompt('enter number');
+// knowOddOrEven(x);
+//-----------------------------------------------------------------------------------------------
+//Q9:
+// let num = prompt("enter number");
+// switch (num%2==0) 
+//         {
+//    case true :
+//    console.log('number is even');
+//    break;
+//    case false :
+//    console.log('number is odd');
+//    break;
+//     }
+//     if (num>20)
+//     {
+//         console.log("you can enter university");
+        
+//     }
+//     else {
+//         console.log("you can not enter");
+        
+//     }
